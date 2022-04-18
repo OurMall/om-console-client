99e54e5fc0c17de5902892ae2b83b57180756602
Brian Castro <bcastro421@misena.edu.co>
2022-04-18 03:03:17 -0500

    Develop: making app navbar and structuring base modules. BC

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
-import { AppRoutingModule } from '@app/app-routing.module';
 import { AppComponent } from '@app/app.component';
+import { SharedModule } from '@shared/shared.module';
+import { AppRoutingModule } from '@app/app-routing.module';
 import { environment } from '@environment/environment';
 
 @NgModule({
 	declarations: [AppComponent],
 	imports: [
 		BrowserModule,
+		BrowserAnimationsModule,
 		AppRoutingModule,
+		HttpClientModule,
+		RouterModule,
+		SharedModule,
 		ServiceWorkerModule.register('ngsw-worker.js', {
 			enabled: environment.production,
 			registrationStrategy: 'registerWhenStable:30000',
 		}),
 	],
-	providers: [],
-	bootstrap: [AppComponent],
+	providers: [
+
+	],
+	bootstrap: [
+		AppComponent
+	],
 })
 export class AppModule {}
