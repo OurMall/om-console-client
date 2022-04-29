7674050da1491fd1c1b270754627968d45f9dcd0
Brian Castro <bcastro421@misena.edu.co>
2022-04-29 02:45:52 -0500

    Add: I'm doing storage service methods. BC

--- src/app/common/services/storage.service.ts
+++ src/app/common/services/storage.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root',
 })
 export class StorageService {
 
-  constructor() { }
+	private storage: Storage;
 
+	constructor(storage: Storage) {
+		this.storage = storage;
+	}
+
+	setItem(key: string, value: any): void {
+		const item: any = JSON.stringify(value);
+		this.storage.setItem(key, item);
+	}
+
+	getItem(key: string): any {
+		const item: any = this.storage.getItem(key);
+		return JSON.parse(item);
+	}
 }
